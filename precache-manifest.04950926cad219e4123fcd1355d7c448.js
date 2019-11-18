a654923a9409fdd0ae8b2bdf6db94d7d48e815c7
speshimen <jirano@kkumail.com>
2019-11-18 17:32:41 +0700

    Updates

---
+++ precache-manifest.04950926cad219e4123fcd1355d7c448.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3280733bbe5416d27595add45bbe6ea",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "758a1512bdee630f8f77",
+    "url": "/test/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "16479721abdafdf23bc8",
+    "url": "/test/static/js/2.55e1b789.chunk.js"
+  },
+  {
+    "revision": "758a1512bdee630f8f77",
+    "url": "/test/static/js/main.c7a9c300.chunk.js"
+  },
+  {
+    "revision": "7e389d36ef167ee0ddc4",
+    "url": "/test/static/js/runtime-main.c779847c.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/test/static/media/logo.25bf045c.svg"
+  }
+]);
